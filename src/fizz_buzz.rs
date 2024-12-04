0de8489208db24a1ca9f3374c62bcd02c69bd382
maskrebel <33626739+maskrebel@users.noreply.github.com>
2024-12-04 15:48:49 +0700

    feat: fizz_buzz

---
+++ src/fizz_buzz.rs
@@ -0,0 +1,19 @@
+fn fizzBuzz(n: i32) {
+    for i in 1..=n {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
+
+#[test]
+fn main() {
+    let n: i32 = 10;
+    fizzBuzz(n);
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod fizz_buzz;
+
+fn main() {
+    println!("Hello, world!");
+}
